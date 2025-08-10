e89e16a1080809ff1de63d649abacc8cf53f1bd2
Ankit Khatua <140873959+oxyboron25@users.noreply.github.com>
2025-08-10 10:39:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,7 @@ export default function Home() {
       <Founders />
       <About />
       <Button />
+      <MentorCarousel />
       <OurBranches />
       <OpenYourBranch />
       {/* <Timeline /> */}
