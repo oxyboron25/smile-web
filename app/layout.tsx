0dd4e6c7471d974c1fb50692c7b6b78972ea6dc7
obliviousorion <obliviousorion@gmail.com>
2025-09-05 20:37:29 +0530

    fix: employed a fix for favicon - waiting for teammates' confirmation

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/new/fav.png" type="image/png" />
       </head>
       <body
         className={
